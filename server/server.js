38290132d74e695b253ce2af7c83291e2b06881c
Tony Braasch <tonybraasch@gmail.com>
2019-04-02 20:52:29 -0500

    update server.js for heroku deployment

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const tasklistRouter = require('./routes/tasklist.router');
 const categoryRouter = require('./routes/category.router');
+const PORT = process.env.PORT || 5000;
 
-const app = express();
-const PORT = 5000;
-
+/** ---------- MIDDLEWARE ---------- **/
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+/** ---------- EXPRESS ROUTES ---------- **/
 app.use('/task', tasklistRouter);
 app.use('/category', categoryRouter);
+
+/** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
 });
