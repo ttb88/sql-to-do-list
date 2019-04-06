d49b48a449586de8d1443d5af3ea24098d988f6a
Tony Braasch <tonybraasch@gmail.com>
2019-04-06 16:44:03 -0500

    update pool.js for heroku deployment

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,19 +1,35 @@
 const pg = require('pg');
+const url = require('url');
+let config = {};
 
-const pool = pg.Pool({ 
-    host: 'localhost',
-    port: 5432, 
-    database: 'weekend-to-do-app',
-    max: 10, 
-    idleTimeoutMillis: 30000
-});
+if (process.env.DATABASE_URL) {
+    // Heroku gives a url, not a connection object
+    // https://github.com/brianc/node-pg-pool
+    let params = url.parse(process.env.DATABASE_URL);
+    let auth = params.auth.split(':');
 
-pool.on('connect', () => {
-    console.log('Postgresql connected');
-});
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: true, // heroku requires ssl to be true
+        max: 10, // max number of clients in the pool
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
 
-pool.on('error', () => {
-    console.log('Error with postgres pool', error);
-});
+} else {
+    // only change the things on the right side of the ||
+    config = {
+        user: process.env.PG_USER || null, //env var: PGUSER
+        password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
+        host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
+        port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
+        database: process.env.DATABASE_NAME || 'prime_feedback', //env var: PGDATABASE or the name of your database (e.g. database: process.env.DATABASE_NAME || 'koala_holla',)
+        max: 10, // max number of clients in the pool
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
+}
 
-module.exports = pool;
+module.exports = new pg.Pool(config);
